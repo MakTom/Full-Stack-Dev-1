f5c7bec0916c1ecdb281f767494c51f2b66b7dd3
Mayank <mayanksae@gmail.com>
2021-09-16 21:51:10 -0400

    Commit 3

--- Lab 4/exercise-3/Ex 3.js
+++ Lab 4/exercise-3/Ex 3.js
@@ -1,5 +1,7 @@
 const say = require('say');
 
-say.speak("Hello");
-say.stop();
-say.speak("Hello?","Alex",0.5);
+function sayHello() {
+    say.speak("I’m sorry, Dave");
+  }
+  
+  setTimeout(sayHello, 5000);
